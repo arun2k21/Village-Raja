75595ea06a9f1b597df3d77c4c23799181c338fc
Arunkumar <93480495+arun2k21@users.noreply.github.com>
2024-04-01 20:58:22 +0530

    New Addons

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 ZOHO.CREATOR.init()
-        .then((data)=> {
+  .then((data) => {
 
-            // var login_user = ZOHO.CREATOR.UTIL.getInitParams();
-            
-            const products = async (category,product)=> {
-                config = {
-                    appName : "village-raja-order-management",
-                reportName : "All_Products",
-                criteria: "Item_Name != null",
-                page : "1",
-                pageSize : "200"
-            }
-             
-            const productsArr = await ZOHO.CREATOR.API.getAllRecords(config);
-           const itemArr = productsArr.data;
-           let card ="";
-           for (i=0;i < itemArr.length; i++) {
-               const img_api = itemArr[i].Item_Image?itemArr[i].Item_Image:"";
-               const product_img ="https://creatorapp.zoho.in"+ img_api;
-                card += `<div class="col-lg-6 col-md-6 col-12 mt-3">
+    // var login_user = ZOHO.CREATOR.UTIL.getInitParams();
+
+    const products = async (category) => {
+      let filter ="Item_Name != null";
+      config = {
+        appName: "village-raja-order-management",
+        reportName: "All_Products",
+        criteria: filter,
+        page: "1",
+        pageSize: "200"
+      }
+
+      const productsArr = await ZOHO.CREATOR.API.getAllRecords(config);
+      const itemArr = productsArr.data;
+      let card = "";
+      for (i = 0; i < itemArr.length; i++) {
+        const img_api = itemArr[i].Item_Image ? itemArr[i].Item_Image : "";
+        const product_img = "https://creatorapp.zoho.in" + img_api;
+        card += `<div class="col-lg-6 col-md-6 col-12 mt-3" id='card-group${i}'>
                <div class="row">
                  <div class="col-12">
                    <div class="card food-card border-0 light">
                      <div class="row">
                        <div class="col-8">
-                         <div class="p-2 fw-bold">
+                         <div class="p-2 fw-bold item-name-0">
                            ${itemArr[i].Item_Name}
                          </div>
                          <div class="fw-bold px-2">₹${itemArr[i].Selling_Price}</div>
@@ -45,31 +46,32 @@ ZOHO.CREATOR.init()
                  </div>
                </div>
              </div>`;
-             const card_group = document.querySelector("#product-card");
-             card_group.innerHTML = card;
-           }
-           const add_cart = await addToCart(itemArr);
-           const item_qty =await itemQty(itemArr);
-            }
-           products();
+        const card_group = document.querySelector("#product-card");
+        card_group.innerHTML = card;
+      }
+      const add_cart = await addToCart(itemArr);
+      const item_qty = await itemQty(itemArr);
+      const search_item = await searchItem(itemArr);
+    }
+    products();
 
-          //  Add Item to cart
-           const addToCart = (itemArr)=>{
-            for (let i = 0; i < itemArr.length; i++) {
-              const addBtn = document.querySelector(`#btn-${i}`);
-              addBtn.addEventListener("click",()=>{
-                const rangeBtn = `<div class="quantity text-white d-flex justify-content-center">
+    //  Add Item to cart
+    const addToCart = (itemArr) => {
+      for (let i = 0; i < itemArr.length; i++) {
+        const addBtn = document.querySelector(`#btn-${i}`);
+        addBtn.addEventListener("click", () => {
+          const rangeBtn = `<div class="quantity text-white d-flex justify-content-center">
                 <button class="border-0 px-2 add-cart dark rounded-start text-white" id='decrease-${i}'>-</button>
                 <div class="p-1 px-2 dark h-100" id='qty${i}' >1</div>
                 <button class="border-0 px-2 add-cart dark rounded-end text-white" id='increase-${i}'>+</button>
             </div>`;
-            const btnType = document.querySelector(`#btn-type${i}`);
-            btnType.innerHTML = rangeBtn;
-            const cartItem = `<div class="row align-items-center">
+          const btnType = document.querySelector(`#btn-type${i}`);
+          btnType.innerHTML = rangeBtn;
+          const cartItem = `<div class="row align-items-center">
             <div class="p-1 col-8">${itemArr[i].Item_Name}</div>
             <div class="p-1 col-2"><div class="w-100 text-center align-items-center">
               <div class="quantity text-white d-flex justify-content-center">
-                  <button class="border-0 px-2 add-cartc dark rounded-start text-white">-</button>
+                  <button class="border-0 px-2 add-cart dark rounded-start text-white">-</button>
                   <div class="p-1 px-2 dark h-100">1</div>
                   <button class="border-0 px-2 add-cart dark rounded-end text-white">+</button>
               </div>
@@ -83,18 +85,65 @@ ZOHO.CREATOR.init()
           list_item.innerHTML = cartItem;
           list_group.appendChild(list_item);
 
-              })
-            }
-           }
+        })
+      }
+    }
 
-          //  Qty Adjuster
-           const itemQty = (itemArr)=>{
-            for (let i = 0; i < itemArr.length; i++) {
-              
-              
-            }
-           }
+    //  Qty Adjuster
+    const itemQty = (itemArr) => {
+      for (let i = 0; i < itemArr.length; i++) {
+
+      }
+    }
+    // Search Bar
+    const searchItem = (itemArr) => {
+      const search = document.querySelector("#search-input");
+      const items = itemArr;
+      search.addEventListener("input", () => {
+        const search = document.querySelector("#search-input");
+        const search_value = search.value.toLowerCase();
+        const item_group = document.getElementsByClassName(`item-name-0`);
+        for (let i = 0; i < item_group.length; i++) {
+          const element = item_group[i];
+          const text_value = element.textContent.toLowerCase();
+          const product = document.querySelector(`#card-group${i}`);
+          if (text_value.includes(search_value)) {
+            
+            product.classList.remove("d-none");
+          }
+          else {
+            product.classList.add("d-none");
+          }
 
 
-          
-        });
+        }
+
+      })
+    }
+
+    // Category Filter
+
+    const getCategory =async (itemArr)=>{
+      config = {
+        appName: "village-raja-order-management",
+        reportName: "All_Categories",
+      }
+      const category_response = await ZOHO.CREATOR.API.getAllRecords(config);
+      const cate_list = category_response.data;
+      const dropdown = document.querySelector("#categories");
+      let new_category = "";
+      for (let i = 0; i < cate_list.length; i++) {
+        const element = cate_list[i];
+         new_category += `<li><a class="dropdown-item cursor-pointer" id="cat-${i}" >${element.Category}</a></li>`;
+      }
+      dropdown.innerHTML = new_category;
+      await catgoryFilter(cate_list,itemArr);
+    }
+
+
+ 
+
+
+
+
+  });
